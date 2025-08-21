e77653093443d18bcf4df3d3e2b16539fc5cc517
Vijay2005it <kvijayakumarit2005@gmail.com>
2025-08-22 00:01:54 +0530

    code updated

--- drums.js
+++ drums.js
@@ -1,6 +1,7 @@
 const Button1 = document.querySelector("#btn1");
 const Button2 = document.querySelector("#btn2");
 const Button3 = document.querySelector("#btn3");
+const Button4 = document.querySelector("#btn4");
 
 function btn1() {
     document.querySelector("#audio1").play();
@@ -13,8 +14,12 @@ function btn2() {
 function btn3() {
     document.querySelector("#audio3").play();
 }
+function btn4(){
+    document.querySelector("#audio4").play();
+}
 
 // Example: attach to click events
 Button1.addEventListener("click", btn1);
 Button2.addEventListener("click", btn2);
 Button3.addEventListener("click", btn3);
+Button4.addEventListener("click", btn4);
